927b357ec727e1d9c7dd6ac526be987c67dbadda
matinabajracharya13 <22252274@students.latrobe.edu.au>
2026-03-17 14:27:52 +1100

    navigation and sms

--- app/app/(tabs)/_layout.tsx
+++ app/app/(tabs)/_layout.tsx
@@ -24,6 +24,13 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+        name="c2"
+        options={{
+          title: 'NewTab',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
---
+++ app/app/(tabs)/c2.tsx
@@ -0,0 +1,22 @@
+import { Background } from '@react-navigation/elements';
+import {Link, Tabs} from 'expo-router';
+import React from 'react';
+import { StyleSheet, View, Text, Pressable, Button } from 'react-native';
+
+export default function ContactScreen2() {
+    return(
+        <Link href="/(tabs)" asChild>
+            <Button title='Index (Back)'/>
+        </Link>
+    )
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 40
+    }
+})
